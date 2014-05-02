6fa46829eeec803f0d9cfd06a0132894e346203f
jsfr <jfrdev@gmail.com>
2014-05-02 21:44:53 +0200

    Finished the shell code for assignment 2

--- handout/week2-shellcode/assignment/shellcode.asm
+++ handout/week2-shellcode/assignment/shellcode.asm
@@ -1,13 +1,14 @@
 bits 32
 %include "include/all.asm"
 
-# Store string on stack
+;; WRITE
+; Push string on stack
 push byte 0x0A
 push `orld`
 push `o, w`
 push `hell`
 
-# Perform write
+; Perform write
 push byte 13
 pop edx
 mov ecx, esp
@@ -16,13 +17,36 @@ push byte SYS_write
 pop eax
 int 0x80
 
-# Perform execve with /usr/bin/id -u
+;; EXECVE
+; Cell two of array
+xor eax, eax
+push eax
+push word `-u`
+mov ecx, esp
 
+; Cell one of array
+push eax
+push byte `d`
+push word `/i`
+push `/bin`
+push `/usr`
+mov ebx, esp
+
+; Create envp and the null pointer of argc
+push eax
+mov edx, esp
+
+; Create argc
+push ecx
+push ebx
+mov ecx, esp
+
+; Perform execve with /usr/bin/id -u
 push byte SYS_execve
 pop eax
 int 0x80
 
-# Exit the program
+; Exit the program
 xor ebx, ebx
 push byte SYS_exit
 pop eax
