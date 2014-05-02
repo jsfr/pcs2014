de13f53e30cae87a26a920706977f405caae76af
jsfr <jfrdev@gmail.com>
2014-05-02 15:39:03 +0200

    Started assignment 2

--- handout/week2-shellcode/assignment/shellcode.asm
+++ handout/week2-shellcode/assignment/shellcode.asm
@@ -1,5 +1,29 @@
 bits 32
-  ;
-  ; REPLACE THIS FILE WITH YOUR OWN
-  ;
-  int3
+%include "include/all.asm"
+
+# Store string on stack
+push byte 0x0A
+push `orld`
+push `o, w`
+push `hell`
+
+# Perform write
+push byte 13
+pop edx
+mov ecx, esp
+xor ebx, ebx
+push byte SYS_write
+pop eax
+int 0x80
+
+# Perform execve with /usr/bin/id -u
+
+push byte SYS_execve
+pop eax
+int 0x80
+
+# Exit the program
+xor ebx, ebx
+push byte SYS_exit
+pop eax
+int 0x80
